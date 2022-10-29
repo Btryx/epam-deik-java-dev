23c279d7404805815dbf836afa49b215ec341147
Oláh Beatrix <beatriix2002@gmail.com>
2022-10-29 13:02:43 +0200

    configure ui, commands for movie commands

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/ui/commands/MovieCommands.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/ui/commands/MovieCommands.java
@@ -1,2 +1,48 @@
-package com.epam.training.ticketservice.ui.commands;public class MovieCommands {
+package com.epam.training.ticketservice.ui.commands;
+
+import com.epam.training.ticketservice.core.movie.MovieService;
+import com.epam.training.ticketservice.core.movie.model.Movie;
+import lombok.AllArgsConstructor;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+@AllArgsConstructor
+public class MovieCommands {
+
+    private final MovieService movieService;
+
+    @ShellMethod(key = "create movie", value = "Create new movie")
+    public String addMovie(String name, String genre, int length){
+        movieService.createMovie(new Movie(name, genre, length));
+        return "New movie created, with the title: " + name + ".";
+    }
+
+    @ShellMethod(key = "update movie", value = "Update existing movie")
+    public String editMovie(String name, String genre, int length){
+        if(movieService.getMovieByName(name) != null){
+            movieService.updateMovie(name, genre, length);
+            return "Movie with title " + name + " updated.";
+        }
+        return "No movie with title " + name + ".";
+    }
+
+    @ShellMethod(key = "delete movie", value = "Update existing movie")
+    public String deleteMovie(String name){
+        Movie movie = movieService.getMovieByName(name);
+        if(movie != null){
+            movieService.deleteMovie(movie);
+            return "Movie with title " + name + " deleted.";
+        }
+        return "No movie with title " + name + ".";
+    }
+
+    @ShellMethod(key = "list movies", value = "List all movies")
+    public String movieList() {
+        if (movieService.getMovieList().isEmpty()) {
+            return "There are no movies at the moment!";
+        } else {
+            return movieService.getMovieList().toString();
+        }
+    }
 }
--- final/ticket-service/src/main/java/com/epam/training/ticketservice/ui/configuration/Prompt.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/ui/configuration/Prompt.java
@@ -1,2 +1,16 @@
-package com.epam.training.ticketservice.ui.configuration;public class Prompt {
+package com.epam.training.ticketservice.ui.configuration;
+
+import org.jline.utils.AttributedString;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.shell.jline.PromptProvider;
+
+import static org.jline.utils.AttributedStyle.DEFAULT;
+import static org.jline.utils.AttributedStyle.GREEN;
+
+@Configuration
+public class Prompt implements PromptProvider {
+    @Override
+    public AttributedString getPrompt() {
+        return new AttributedString("Ticket service>", DEFAULT.foreground(GREEN));
+    }
 }
